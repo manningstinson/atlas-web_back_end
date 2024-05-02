3ecc8e8ef00ed6a7d4ffa7d8b43c5bf32dcca3eb
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-05-02 14:22:32 +0000

    updated package.json

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+    const task = false;
+    const task2 = true;
 
-  if (trueOrFalse) {
-   const task = true;  // Updated value without re-declaring
-   const task2 = false;  // Updated value without re-declaring
-  }
+    if (trueOrFalse) {
+        const task = true;  // Updated value without re-declaring
+        const task2 = false;  // Updated value without re-declaring
+    }
 
-  return [task, task2];
+    return [task, task2];
 }
