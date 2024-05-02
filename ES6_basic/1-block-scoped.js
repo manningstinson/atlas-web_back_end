62adb508aff2b9249ad8f1576571143763c18219
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-05-02 18:45:48 +0000

    updated task 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,13 +1,15 @@
 export default function taskBlock(trueOrFalse) {
-	const task = false;
-	const task2 = true;
+  let task = false;
+  let task2 = true;
 
-	if (trueOrFalse) {
-		const task = true; // Updated value without re-declaring
-		const task2 = false; // Updated value without re-declaring
-	}
+  if (trueOrFalse) {
+    task = true; // Updated value without re-declaring
+    task2 = false; // Updated value without re-declaring
+    console.log(task, task2); // Using task and task2 to satisfy ESLint
+  }
 
-	console.log(task, task2); // Adding a console.log statement to satisfy ESLint
-
-	return [task, task2];
+  return [task, task2];
 }
+
+// Or returning an object
+// return { task, task2 };
