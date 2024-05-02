7f2a1ad328835956bb9c49c670f3565157ddce38
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-05-02 17:53:18 +0000

    updated task 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -7,5 +7,5 @@ export default function taskBlock(trueOrFalse) {
 		const task2 = false; // Updated value without re-declaring
 	}
 
-	console.log(task, task2); // Use parentheses instead of square brackets
+	console.log(task, task2); // Using task and task2 to resolve ESLint errors
 }
