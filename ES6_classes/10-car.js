2e782b3c3ad6eb5f98a1c5b042c7b9250962fde2
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-05-06 15:06:57 +0000

    updated task 10

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,23 +1,23 @@
 // 10-car.js
 export default class Car {
-	constructor(brand, motor, color) {
-		this._brand = brand;
-		this._motor = motor;
-		this._color = color;
-	}
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
 
-	// Define getter for Symbol.species
-	static get [Symbol.species]() {
-		// Ensure that cloneCar method returns an instance of the same class it is called on
-		return this;
-	}
+  // Define getter for Symbol.species
+  static get [Symbol.species]() {
+    // Ensure that cloneCar method returns an instance of the same class it is called on
+    return this;
+  }
 
-	cloneCar() {
-		// Use Symbol.species to create a new instance
-		return new this.constructor[Symbol.species](
-			this._brand,
-			this._motor,
-			this._color
-		);
-	}
+  cloneCar() {
+    // Use Symbol.species to create a new instance
+    return new this.constructor[Symbol.species](
+      this.brand,
+      this.motor,
+      this.color,
+    );
+  }
 }
