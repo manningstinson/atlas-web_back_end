1ebb6524e84844d1c0af1186684f1048616185e0
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-05-06 17:33:34 +0000

    updated task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,18 @@
-// 5-building.js
-export default class Building {
-  constructor(sqft) {
-    this._sqft = sqft;
-  }
+// 5-main.js
+import Building from "./5-building.js";
 
-  get sqft() {
-    return this._sqft;
-  }
-
-  // Abstract method
-  evacuationWarningMessage() {
-    throw new Error(
-      'Class extending Building must override evacuationWarningMessage',
-    );
-  }
+class TestBuilding extends Building {
+	// Override the abstract method
+	evacuationWarningMessage() {
+		// Provide a custom implementation here
+		return "Custom evacuation warning message";
+	}
 }
+
+test("Building forces override", () => {
+	expect(() => {
+		new TestBuilding(200);
+	}).toThrowError(
+		"Class extending Building must override evacuationWarningMessage"
+	);
+});
