fda1242a233563c957aa4d671c073ff71c034ac9
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-05-06 20:11:14 +0000

    updated task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,15 +1,20 @@
 class Building {
-    constructor(sqft) {
-        this._sqft = sqft;
-    }
+	constructor(sqft) {
+		if (
+			this.constructor !== Building &&
+			typeof this.evacuationWarningMessage !== "function"
+		) {
+			throw Error(
+				"Class extending Building must override evacuationWarningMessage"
+			);
+		}
+		this._sqft = sqft;
+	}
 
-    get sqft() {
-        return this._sqft;
-    }
-
-    evacuationWarningMessage = () => {
-        throw new Error("Class extending Building must override evacuationWarningMessage");
-    };
+	// sqft
+	get sqft() {
+		return this._sqft;
+	}
 }
 
 export default Building;
