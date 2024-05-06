2c06c2cd1e450ff5d39f32fbc5ae9aa6d4acbf7c
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-05-06 19:36:01 +0000

    updated task6

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,22 +1,18 @@
+// 6-sky_high.js
 import Building from './5-building';
 
-class SkyHighBuilding extends Building {
-  constructor(sqft, floors) {
-    super(sqft);
-    this._floors = floors;
-  }
+export default class SkyHighBuilding extends Building {
+    constructor(sqft, floors) {
+        super(sqft); // Call the parent class constructor with sqft
+        this._floors = floors;
+    }
 
-  get sqft() {
-    return this._sqft;
-  }
+    get floors() {
+        return this._floors;
+    }
 
-  get floors() {
-    return this._floors;
-  }
-
-  evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors`;
-  }
+    // Override the evacuationWarningMessage method
+    evacuationWarningMessage() {
+        return `Evacuate slowly the ${this._floors} floors`;
+    }
 }
-
-export default SkyHighBuilding;
