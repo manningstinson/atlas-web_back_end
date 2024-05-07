1144e3d56f7097f66bb236c9fdc25245c33be9ce
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-05-07 14:20:30 +0000

    updated task 0

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,12 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Make API call or perform asynchronous task here
+    // For demonstration purposes, let's resolve the promise with a sample response
+    setTimeout(() => {
+      const responseData = { data: "Sample response from API" };
+      resolve(responseData);
+    }, 2000); // Simulating a delay of 2 seconds
+  });
+}
+
+export default getResponseFromAPI;
