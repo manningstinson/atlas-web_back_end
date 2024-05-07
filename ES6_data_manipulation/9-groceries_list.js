fedd749c359827122b7690ab5357be45f3c24386
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-05-07 14:12:57 +0000

    updated task 9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+// 9-groceries_list.js
+function groceriesList() {
+  const groceries = new Map();
+
+  // Add groceries items with their quantities to the map
+  groceries.set('Apples', 10);
+  groceries.set('Tomatoes', 10);
+  groceries.set('Pasta', 1);
+  groceries.set('Rice', 1);
+  groceries.set('Banana', 5);
+
+  return groceries;
+}
+
+export default groceriesList;
