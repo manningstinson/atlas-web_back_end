24a67ef578f3785e43366fd9f26a8f782bcae7c5
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-05-07 19:00:53 +0000

    updated task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,11 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 async function handleProfileSignup() {
   try {
     const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
-    console.log(`${user.firstName} ${user.lastName}`);
+    const { body } = photo;
+    const { firstName, lastName } = user;
+    console.log(`${body} ${firstName} ${lastName}`);
   } catch (error) {
     console.log('Signup system offline');
   }
