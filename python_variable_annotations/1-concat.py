1e950322de9fbe5f1a3e53833562ab955403bca2
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-08-27 15:39:38 +0000

    updated task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module for string concatenation with type annotations.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings & returns the result as a single string.
+    """
+    return str1 + str2
