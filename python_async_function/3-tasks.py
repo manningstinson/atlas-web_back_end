84af8c6f133fec691f6941f9058d2b5ab53f3473
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-08-27 16:57:28 +0000

    updated task 3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
-
+"""
+This module defines a function
+that returns an asyncio.Task for the wait_random coroutine.
+"""
 import asyncio
-task_wait_random = __import__('3-tasks').task_wait_random
+from typing import Union
+
+# Importing wait_random from 0-basic_async_syntax.py using __import__()
+wait_random = __import__('0_basic_async_syntax').wait_random
 
 
-async def test(max_delay: int) -> float:
-    task = task_wait_random(max_delay)
-    await task
-    print(task.__class__)
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Returns an asyncio.Task for wait_random.
 
-asyncio.run(test(5))
+    Args:
+        max_delay (int): Maximum delay in seconds.
+
+    Returns:
+        asyncio.Task: The asyncio.Task
+        object that runs the wait_random coroutine.
+    """
+    return asyncio.create_task(wait_random(max_delay))
