cf1f86445b44127a0cd87a62a7c9b18dcaaaa9d6
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-08-27 15:26:48 +0000

    updated task 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module for basic arithmetic function with type annotations.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers and returns their sum as a float.
+    """
+    return a + b
