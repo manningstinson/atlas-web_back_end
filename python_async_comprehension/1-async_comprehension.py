dc8a1c27d3f74fcbc21d7a98e81dbb7ac257eb57
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-08-28 19:49:02 +0000

    updated task 0-3

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 """
-This module contains a coroutine
-that collects random numbers
-using asynchronous comprehension.
+This module contains a coroutine that collects random numbers using asynchronous comprehension.
 """
 
 from typing import List
-from 0_async_generator import async_generator  # Absolute import
+
+# Using __import__ to dynamically import the module with a name starting with a digit
+async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """
-    Collects 10 random numbers from
-    the async_generator using an asynchronous comprehension.
+    Collects 10 random numbers from the async_generator using an asynchronous comprehension.
     Returns a list of these numbers.
     """
     return [number async for number in async_generator()]
