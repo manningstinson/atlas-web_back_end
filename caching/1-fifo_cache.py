32d87813f6fe0d03e2d0e1bd476cca7d74be862c
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-09-06 15:52:36 +0000

    updated task 1

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python3
-""" LIFOCache module """
+""" FIFOCache module """
 
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
-    """ LIFOCache that uses the LIFO caching algorithm """
+class FIFOCache(BaseCaching):
+    """ FIFOCache that uses the FIFO caching algorithm """
 
     def __init__(self):
         """ Initialize the cache """
         super().__init__()
-        self.last_key = None
+        self.keys_order = []
 
     def put(self, key, item):
         """ Add an item in the cache """
         if key is not None and item is not None:
+            if key not in self.cache_data:
+                self.keys_order.append(key)
             self.cache_data[key] = item
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                print(f"DISCARD: {self.last_key}")
-                del self.cache_data[self.last_key]
-            self.last_key = key
+                first_key = self.keys_order.pop(0)
+                del self.cache_data[first_key]
+                print(f"DISCARD: {first_key}")
 
     def get(self, key):
         """ Get an item by key """
