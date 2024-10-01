6ccba637bbcc77858ca9195aa0e83c8315819341
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-10-01 20:19:13 +0000

    updated task 2

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,13 +1,7 @@
--- Import the metal_bands.sql data
--- Note: The actual import command should be run in your terminal, not in the SQL file.
-
--- Use this command in the terminal:
--- cat metal_bands.sql | mysql -uroot -p holberton
-
--- Once the data is imported, run the following query to rank the origins by the number of fans.
-
--- Select the origin of the bands and sum the number of fans
+-- Select the origin of the bands and sum the number of fans,
+-- ensuring to exclude NULL values in the nb_fans column
 SELECT origin, SUM(nb_fans) AS nb_fans
-FROM metal_bands  -- Assuming the data was imported into a table named 'metal_bands'
-GROUP BY origin   -- Group by the country of origin
+FROM metal_bands
+WHERE nb_fans IS NOT NULL  -- Filter out any NULL fan counts
+GROUP BY origin
 ORDER BY nb_fans DESC;  -- Order the results by the number of fans in descending order
