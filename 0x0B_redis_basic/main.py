50100ea2ba89f9791b4ee827e80e2639d543acd6
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-10-01 19:14:56 +0000

    upupdated task 0

--- 0x0B_redis_basic/main.py
+++ 0x0B_redis_basic/main.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
-""" Main file """
+"""
+Main script to run the Cache and replay functionalities.
+"""
 
-Cache = __import__('exercise').Cache
+from exercise import Cache
 
-cache = Cache()
+def main():
+    # Create a cache instance
+    cache = Cache()
 
-# Store some data
-cache.store("foo")
-cache.store("bar")
-cache.store(42)
+    # Store some data
+    cache.store("foo")
+    cache.store("bar")
+    cache.store("egg")
+    cache.store("eggsperiment")
 
-# Replay the method call history
-cache.replay(cache.store)
+    # Replay the calls to the store method
+    cache.replay(cache.store)
+
+if __name__ == "__main__":
+    main()
