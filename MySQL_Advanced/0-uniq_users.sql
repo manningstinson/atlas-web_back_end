92810cafe63e6c3482f7cd3e4fa3703c27269fad
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-10-01 19:26:48 +0000

    updated task 0 with comments

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,16 @@
+-- Create the 'users' table if it doesn't already exist
 CREATE TABLE IF NOT EXISTS users (
+    -- 'id' is an integer that cannot be null and will auto-increment with each new record
     id INT NOT NULL AUTO_INCREMENT,
+
+    -- 'email' is a string with a maximum length of 255 characters
+    -- It cannot be null and must be unique across the table
     email VARCHAR(255) NOT NULL UNIQUE,
+
+    -- 'name' is a string with a maximum length of 255 characters
+    -- It is allowed to be null
     name VARCHAR(255),
+
+    -- Define 'id' as the primary key for the table
     PRIMARY KEY (id)
 );
