cfbb1251924762e7f5442feeca2e3ee4e21fcac4
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-10-03 14:25:06 +0000

    updated task 5

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,20 +1,14 @@
--- 5-valid_email.sql
+-- Drop the trigger if it already exists to avoid duplication
+DROP TRIGGER IF EXISTS email_change_trigger;
 
--- Drop the trigger if it already exists
-DROP TRIGGER IF EXISTS reset_valid_email;
-
--- Create the trigger
-DELIMITER //
-
-CREATE TRIGGER reset_valid_email
+-- Create a new trigger that fires BEFORE an update on the users table
+CREATE TRIGGER email_change_trigger
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     -- Check if the email is being changed
-    IF NEW.email != OLD.email THEN
+    IF NEW.email <> OLD.email THEN
         -- Reset valid_email to 0 if the email is changed
         SET NEW.valid_email = 0;
     END IF;
-END //
-
-DELIMITER ;
+END;
