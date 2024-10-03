59c8f130181ce1a2d0d70c7a1666ca1c0edd2686
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-10-03 14:54:31 +0000

    updated task 2

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,7 +1,6 @@
 -- Select the origin of the bands and sum the number of fans,
 -- ensuring to exclude NULL values in the nb_fans column
-SELECT origin, SUM(nb_fans) AS nb_fans
+SELECT origin, AS origin, SUM(nb_fans) AS nb_fans
 FROM metal_bands
-WHERE nb_fans IS NOT NULL  -- Filter out any NULL fan counts
 GROUP BY origin
 ORDER BY nb_fans DESC;  -- Order the results by the number of fans in descending order
