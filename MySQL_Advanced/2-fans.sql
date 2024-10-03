d114cce4368d0672bcdaa1fe3c704c5ea97a211d
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-10-03 15:49:07 +0000

    updated task 2

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,4 +1,7 @@
-SELECT origin AS origin, SUM(nb_fans) AS nb_fans 
+-- Script to rank country origins of bands by the total number of non-unique fans
+
+SELECT origin AS origin, 
+       SUM(nb_fans) AS nb_fans 
 FROM metal_bands 
 GROUP BY origin 
 ORDER BY nb_fans DESC;
