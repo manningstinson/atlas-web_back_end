0410882683e6122ab074b0a146a3ffb64a5f0656
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-10-05 15:28:01 +0000

    updated task 9

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+def insert_school(mongo_collection, **kwargs):
+    """Insert a new document in a collection based on kwargs.
+
+    Args:
+        mongo_collection: The pymongo collection object.
+        **kwargs: The fields for the new document.
+
+    Returns:
+        The new document's _id.
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
