921b15134aa87636fc5ed228f5e0e095559dcc51
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-10-15 13:06:06 +0000

    updated task 3

---
+++ unittests_in_js/3-payment.test.js
@@ -0,0 +1,16 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should use Utils.calculateNumber with correct arguments', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    
+    sendPaymentRequestToApi(100, 20);
+    
+    expect(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    
+    calculateNumberSpy.restore();
+  });
+});
