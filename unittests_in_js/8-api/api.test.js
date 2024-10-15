6531ac9ed473465b6516278130746a492cbe5a50
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-10-15 14:17:51 +0000

    updated task 8 api, and node modules

---
+++ unittests_in_js/8-api/api.test.js
@@ -0,0 +1,18 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+  it('should return status code 200', (done) => {
+    request('http://localhost:7865', (error, response) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the correct message', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
